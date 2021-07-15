bf822e1c4a9198339e5dc0dfaa1364d1b582c4d9
Yuri Mikushov <mikushov.yu@gmail.com>
2021-07-15 12:02:56 +0300

    refactor: wrapped `Checkbox` component

--- src/components/Checkbox/Checkbox.props.ts
+++ src/components/Checkbox/Checkbox.props.ts
@@ -1,6 +1,6 @@
 import { DetailedHTMLProps, HTMLAttributes } from 'react'
 
-interface CheckboxProps extends DetailedHTMLProps<HTMLAttributes<HTMLLabelElement>, HTMLLabelElement> {
+interface CheckboxProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
   checked: boolean,
   title: string,
   onChange: () => void
--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -9,11 +9,14 @@ const Checkbox = ({
   onChange,
   ...props
 }: CheckboxProps) => (
-  <label className={cn(className, 'checkbox')} {...props}>
-    <input type='checkbox' checked={checked} onChange={onChange} />
-    {title}
-    <span className='checkbox__box'></span>
-  </label>
+  // FIXME: should remove double handling `on change` event
+  <div className={cn(className, 'checkbox')} onClick={onChange} {...props}>
+    <label className='checkbox__label'>
+      <input type='checkbox' checked={checked} onChange={onChange} />
+      {title}
+      <span className='checkbox__box'></span>
+    </label>
+  </div>
 )
 
 export { Checkbox }
