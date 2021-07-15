74b4fdfb192eb2d9af031d28bff147bd2b5bcaf0
Yuri Mikushov <mikushov.yu@gmail.com>
2021-07-15 10:39:20 +0300

    feat: made layout

--- src/App.tsx
+++ src/App.tsx
@@ -19,30 +19,65 @@ const App = () => {
     )
 
   return (
-    <div style={{ padding: '20px 0', maxWidth: '600px', margin: '0 auto' }}>
-      <Card style={{ width: '400px', padding: '20px' }}>
-        <div className='greeting'>Hello, see ya soon</div>
-      </Card>
-      <Button style={{ width: '400px' }} onClick={() => console.log('Click')}>
-        Показать еще 5 билетов!
-      </Button>
-      <Card style={{ width: '400px', padding: '20px' }}>
-        <Checkbox
-          title='Все'
-          checked={checked}
-          onChange={() => {
-            setChecked((checked) => !checked)
-          }}
-        />
-        <Checkbox
-          title='Без пересадок'
-          checked={checked}
-          onChange={() => {
-            setChecked((checked) => !checked)
-          }}
-        />
-      </Card>
-      <Tabs tabs={tabs} onSwitch={onSwitchHandler} />
+    <div className='container'>
+      <div className='logo'>Logo</div>
+      <aside>
+        <Card>
+          <div className='filter' style={{ width: '232px', height: '252px' }}>
+            <div>Количество пересадок</div>
+            <Checkbox
+              title='Все'
+              checked={checked}
+              onChange={() => {
+                setChecked((checked) => !checked)
+              }}
+            />
+            <Checkbox
+              title='Без пересадок'
+              checked={checked}
+              onChange={() => {
+                setChecked((checked) => !checked)
+              }}
+            />
+          </div>
+        </Card>
+      </aside>
+      <main>
+        <Tabs tabs={tabs} onSwitch={onSwitchHandler} />
+        <Card>
+          <div
+            className='ticket'
+            style={{ width: '502px', height: '184px' }}
+          ></div>
+        </Card>
+        <Card>
+          <div
+            className='ticket'
+            style={{ width: '502px', height: '184px' }}
+          ></div>
+        </Card>
+        <Card>
+          <div
+            className='ticket'
+            style={{ width: '502px', height: '184px' }}
+          ></div>
+        </Card>
+        <Card>
+          <div
+            className='ticket'
+            style={{ width: '502px', height: '184px' }}
+          ></div>
+        </Card>
+        <Card>
+          <div
+            className='ticket'
+            style={{ width: '502px', height: '184px' }}
+          ></div>
+        </Card>
+        <Button style={{ width: '100%' }} onClick={() => console.log('Click')}>
+          Показать еще 5 билетов!
+        </Button>
+      </main>
     </div>
   )
 }
