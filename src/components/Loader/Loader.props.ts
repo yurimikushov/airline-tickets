e0b40ed80725c67aec0aab57a73f968323687762
Yuri Mikushov <mikushov.yu@gmail.com>
2021-07-16 10:10:03 +0300

    feat: added Loader component

---
+++ src/components/Loader/Loader.props.ts
@@ -0,0 +1,5 @@
+import { DetailedHTMLProps, HTMLAttributes } from 'react'
+
+interface LoaderProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {}
+
+export type { LoaderProps }
---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,11 @@
+import cn from 'classnames'
+import { LoaderProps } from './Loader.props'
+import './Loader.css'
+
+const Loader = ({ className, ...props }: LoaderProps): JSX.Element => (
+  <div className={cn(className, 'loader')} {...props}>
+    Loading...
+  </div>
+)
+
+export { Loader }
