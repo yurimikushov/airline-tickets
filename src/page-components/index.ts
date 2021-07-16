7956098ecb0aab123a52ceb2f63e372995a4130b
Yuri Mikushov <mikushov.yu@gmail.com>
2021-07-16 11:09:01 +0300

    refactor: extracted `SortTickets` component

--- src/page-components/index.ts
+++ src/page-components/index.ts
@@ -1,3 +1,4 @@
 export { Filter } from './Filter/Filter'
 export { Ticket } from './Ticket/Ticket'
 export { Tickets } from './Tickets'
+export { SortTickets } from './SortTickets'
--- src/pages/Tickets.tsx
+++ src/pages/Tickets.tsx
@@ -1,20 +1,15 @@
-import { useTicketsSort } from '../hooks'
 import { TicketsLayout } from '../layouts'
-import { Tickets } from '../page-components'
-import { Button, Tabs } from '../components'
+import { SortTickets, Tickets } from '../page-components'
+import { Button } from '../components'
 
-const TicketsPage = (): JSX.Element => {
-  const [sortTabs, onSortHandler] = useTicketsSort()
-
-  return (
-    <TicketsLayout>
-      <Tabs tabs={sortTabs} onSwitch={onSortHandler} />
-      <Tickets />
-      <Button onClick={() => console.log('Click')}>
-        Показать еще 5 билетов!
-      </Button>
-    </TicketsLayout>
-  )
-}
+const TicketsPage = (): JSX.Element => (
+  <TicketsLayout>
+    <SortTickets />
+    <Tickets />
+    <Button onClick={() => console.log('Click')}>
+      Показать еще 5 билетов!
+    </Button>
+  </TicketsLayout>
+)
 
 export { TicketsPage }
