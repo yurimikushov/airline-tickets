4296d0dffe82ad02a3b829a40f3831ef4e8b01c4
Yuri Mikushov <mikushov.yu@gmail.com>
2021-07-16 13:19:10 +0300

    feat: made btn is disabled when tickets fetch

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,21 @@
 import cn from 'classnames'
+import { dummy } from '../../utils'
 import { ButtonProps } from './Button.props'
 import './Button.css'
 
 const Button = ({
   className,
+  pending = false,
+  onClick,
   children,
   ...props
 }: ButtonProps): JSX.Element => (
-  <button className={cn(className, 'btn')} {...props}>
-    {children}
+  <button
+    className={cn(className, 'btn', pending && 'btn_pending')}
+    onClick={!pending ? onClick : dummy}
+    {...props}
+  >
+    {!pending ? children : 'Загрузка...'}
   </button>
 )
 
---
+++ src/utils/dummy.ts
@@ -0,0 +1,3 @@
+const dummy = (): void => {}
+
+export { dummy }
