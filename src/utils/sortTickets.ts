92bc93fc271ea79c61217bd380ffcbb0bfe35e9f
Yuri Mikushov <mikushov.yu@gmail.com>
2021-07-18 09:38:15 +0300

    refactor: extracted sort sub funcs

--- src/utils/sortTickets.ts
+++ src/utils/sortTickets.ts
@@ -2,35 +2,39 @@ import { SORT } from '../constants'
 import { ISort, ITicket } from '../interfaces'
 import { calcTotalFlightDuration } from './calcFlightDuration'
 
+const sortByPrice = (a: ITicket, b: ITicket): number => {
+  if (a.price < b.price) {
+    return -1
+  }
+
+  if (a.price === b.price) {
+    return 0
+  }
+
+  return 1
+}
+
+const sortByFlightDuration = (a: ITicket, b: ITicket): number => {
+  const aDuration = calcTotalFlightDuration(a)
+  const bDuration = calcTotalFlightDuration(b)
+
+  if (aDuration < bDuration) {
+    return -1
+  }
+
+  if (aDuration === bDuration) {
+    return 0
+  }
+
+  return 1
+}
+
 const sortTickets = (tickets: ITicket[], sort: ISort): ITicket[] => {
   switch (sort.title) {
     case SORT.CHEEP:
-      return tickets.sort((a, b): number => {
-        if (a.price < b.price) {
-          return -1
-        }
-
-        if (a.price === b.price) {
-          return 0
-        }
-
-        return 1
-      })
+      return tickets.sort(sortByPrice)
     case SORT.FAST:
-      return tickets.sort((a, b): number => {
-        const aDuration = calcTotalFlightDuration(a)
-        const bDuration = calcTotalFlightDuration(b)
-
-        if (aDuration < bDuration) {
-          return -1
-        }
-
-        if (aDuration === bDuration) {
-          return 0
-        }
-
-        return 1
-      })
+      return tickets.sort(sortByFlightDuration)
     case SORT.OPTIMAL:
       return tickets
     default:
