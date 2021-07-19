3ce0694bc2ea5b0fd51158f09ec5a095c0a8fc74
Yuri Mikushov <mikushov.yu@gmail.com>
2021-07-19 11:43:35 +0300

    refactor: moved logic of re-try tickets fetching to `saga`

--- src/api/fetchTickets.ts
+++ src/api/fetchTickets.ts
@@ -1,29 +1,7 @@
-import { MAX_NUM_OF_TRY_FETCHING } from '../constants'
 import { IFetchTicketsResponse } from '../interfaces'
 import { addUniqueId } from '../utils'
 
 const fetchTickets = async (
-  searchId: string
-): Promise<IFetchTicketsResponse> => {
-  return await tryFetchTickets(searchId)
-}
-
-const tryFetchTickets = async (
-  searchId: string,
-  tryCounter = 1
-): Promise<IFetchTicketsResponse> => {
-  try {
-    return await handleFetchTickets(searchId)
-  } catch (err) {
-    if (tryCounter < MAX_NUM_OF_TRY_FETCHING) {
-      return await tryFetchTickets(searchId, tryCounter + 1)
-    }
-
-    throw err
-  }
-}
-
-const handleFetchTickets = async (
   searchId: string
 ): Promise<IFetchTicketsResponse> => {
   const res = await fetch(
--- src/store/sagas/watchFetchTickets.ts
+++ src/store/sagas/watchFetchTickets.ts
@@ -1,5 +1,6 @@
 import { takeEvery, call, put, select } from 'redux-saga/effects'
 import { fetchSearchId, fetchTickets } from '../../api'
+import { MAX_NUM_OF_TRY_FETCHING } from '../../constants'
 import { IFetchTicketsResponse, IFilter, ITicket } from '../../interfaces'
 import { filterAndSliceTickets } from '../../utils'
 import {
@@ -20,7 +21,8 @@ function* handleFetchTickets() {
     yield put(startFetchTickets())
 
     const searchId: string = yield call(getSearchId)
-    const tickets: IFetchTicketsResponse = yield call(fetchTickets, searchId)
+    const tickets: IFetchTicketsResponse = yield call(fetchTicketsApi, searchId)
+
     const filteredTickets: ITicket[] = yield call(
       filterTickets,
       tickets.tickets
@@ -49,6 +51,17 @@ function* getSearchId() {
   }
 
   return searchId
+}
+
+function* fetchTicketsApi(searchId: string) {
+  for (let tryCounter = 0; tryCounter < MAX_NUM_OF_TRY_FETCHING; tryCounter++) {
+    try {
+      const tickets: IFetchTicketsResponse = yield call(fetchTickets, searchId)
+      return tickets
+    } catch {}
+  }
+
+  throw new Error('Не удалось выполнить запрос')
 }
 
 function* filterTickets(tickets: ITicket[]) {
