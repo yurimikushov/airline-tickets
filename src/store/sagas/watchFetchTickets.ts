06a4e14ee4a8c16acf64641407df4700e395208c
Yuri Mikushov <mikushov.yu@gmail.com>
2021-07-20 14:09:58 +0300

    refactor: embedded filtering and slicing of tickets

--- src/store/sagas/watchFetchTickets.ts
+++ src/store/sagas/watchFetchTickets.ts
@@ -2,7 +2,7 @@ import { takeEvery, call, put, select, retry } from 'redux-saga/effects'
 import { fetchSearchId, fetchTickets } from '../../api'
 import { MAX_NUM_OF_TRY_FETCHING } from '../../constants'
 import { IFetchTicketsResponse, IFilter, ITicket } from '../../interfaces'
-import { filterAndSliceTickets } from '../../utils'
+import { filterAndSliceTickets as filterAndSliceNeedlessTickets } from '../../utils'
 import {
   addTickets,
   endFetchTickets,
@@ -21,17 +21,21 @@ function* handleFetchTickets() {
     yield put(startFetchTickets())
 
     const searchId: string = yield call(getSearchId)
-    const tickets: IFetchTicketsResponse = yield call(fetchTicketsApi, searchId)
+    const { tickets, stop }: IFetchTicketsResponse = yield call(
+      fetchTicketsApi,
+      searchId
+    )
 
-    const filteredTickets: ITicket[] = yield call(
-      filterAndSliceNeedlessTickets,
-      tickets.tickets
+    const filters: IFilter[] = yield select(checkedFiltersSelector)
+    const filteredTickets: ITicket[] = filterAndSliceNeedlessTickets(
+      tickets,
+      filters
     )
 
     yield put(
       addTickets({
-        ...tickets,
         tickets: filteredTickets,
+        stop,
       })
     )
     yield put(endFetchTickets())
@@ -66,12 +70,6 @@ function* fetchTicketsApi(searchId: string) {
   } catch {
     throw new Error('Не удалось выполнить запрос')
   }
-}
-
-function* filterAndSliceNeedlessTickets(tickets: ITicket[]) {
-  const filters: IFilter[] = yield select(checkedFiltersSelector)
-
-  return filterAndSliceTickets(tickets, filters)
 }
 
 export { watchFetchTickets, handleFetchTickets }
