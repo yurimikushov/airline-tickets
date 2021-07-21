00657a2361602af766f2265ec16ad61606f0c170
Yuri Mikushov <mikushov.yu@gmail.com>
2021-07-21 21:51:29 +0300

    chore: added tests for `handleFetchTickets()`

--- src/store/sagas/__tests__/watchFetchTickets.test.ts
+++ src/store/sagas/__tests__/watchFetchTickets.test.ts
@@ -1,6 +1,16 @@
-import { takeEvery } from "@redux-saga/core/effects"
-import { TICKETS } from "../../actionTypes"
-import { handleFetchTickets, watchFetchTickets } from "../watchFetchTickets"
+import { call, select, takeEvery } from '@redux-saga/core/effects'
+import { expectSaga, testSaga } from 'redux-saga-test-plan'
+import { IFetchTicketsResponse, IFilter } from '../../../interfaces'
+import { filterAndSliceNeedlessTickets } from '../../../utils'
+import { addTickets, endFetchTickets, startFetchTickets } from '../../actions'
+import { TICKETS } from '../../actionTypes'
+import { checkedFiltersSelector } from '../../selectors'
+import {
+  watchFetchTickets,
+  handleFetchTickets,
+  getSearchId,
+  fetchTicketsApi,
+} from '../watchFetchTickets'
 
 describe('check `watchFetchTickets()`', () => {
   const gen = watchFetchTickets()
@@ -9,5 +19,83 @@ describe('check `watchFetchTickets()`', () => {
     expect(gen.next().value).toEqual(
       takeEvery(TICKETS.FETCH, handleFetchTickets)
     )
+  })
+})
+
+describe('check `handleFetchTickets()`', () => {
+  const mockSearchId = '1234'
+  const mockFetchResponse: IFetchTicketsResponse = {
+    tickets: [
+      {
+        id: 'id-3000',
+        price: 10500,
+        carrier: 'S7',
+        segments: [],
+      },
+    ],
+    stop: false,
+  }
+
+  test('should fetch and dispatch tickets success (1)', () => {
+    return expectSaga(handleFetchTickets)
+      .provide([
+        [call(getSearchId), mockSearchId],
+        [call(fetchTicketsApi, mockSearchId), mockFetchResponse],
+        [select(checkedFiltersSelector), []],
+        [
+          call(filterAndSliceNeedlessTickets, mockFetchResponse.tickets, []),
+          [],
+        ],
+      ])
+      .put(startFetchTickets())
+      .call(getSearchId)
+      .call(fetchTicketsApi, mockSearchId)
+      .select(checkedFiltersSelector)
+      .put(
+        addTickets({
+          tickets: [],
+          stop: false,
+        })
+      )
+      .put(endFetchTickets())
+      .run()
+  })
+
+  test('should fetch and dispatch tickets success (2)', () => {
+    const mockFilters: IFilter[] = []
+
+    testSaga(handleFetchTickets)
+      .next()
+      .put(startFetchTickets())
+      .next()
+      .call(getSearchId)
+      .save('before tickets fetching')
+      .next(mockSearchId)
+      .call(fetchTicketsApi, mockSearchId)
+      .next(mockFetchResponse)
+      .select(checkedFiltersSelector)
+      .next([])
+      .call(
+        filterAndSliceNeedlessTickets,
+        mockFetchResponse.tickets,
+        mockFilters
+      )
+      .next(mockFetchResponse.tickets)
+      .put(
+        addTickets({
+          tickets: mockFetchResponse.tickets,
+          stop: false,
+        })
+      )
+      .next()
+      .put(endFetchTickets())
+      .next()
+      .isDone()
+      .restore('before tickets fetching')
+      .next(mockSearchId)
+      .throw(new Error('suppose fetch is failed'))
+      .put(endFetchTickets('Что-то пошло не так. Попробуйте обновить страницу'))
+      .next()
+      .isDone()
   })
 })
